d82d99e4858e14b83f89223386476f678425f23c
Ubuntu <ubuntu@ip-172-31-30-186.ec2.internal>
2019-11-02 11:50:38 +0000

    update

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
+# User has many tasks
+# A specific task belong to a user
 class User < ApplicationRecord
   has_secure_password
+  has_many :tasks
+  validates :username, uniqueness: {case_sensitive: false}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
+
   resources :users
   get 'tasks/index'
 
@@ -6,6 +8,15 @@ Rails.application.routes.draw do
 
   root 'welcome#index'
   get 'welcome/index'
+  
+  resources :sessions, only: [:new, :create, :destroy]
+  get 'login', to: 'sessions#new'
+  get 'logout', to: 'sessions#destroy'
+  
+  resources :users
+  get 'signup', to: 'users#new'
+  
+  
   resources:tasks
   get '/tasks/:id/complete/:completed', to:'tasks#complete'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
